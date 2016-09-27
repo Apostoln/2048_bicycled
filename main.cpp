fd205b93369c286f32f204229715b3d1d451fe0f
Apostoln <my@example.com>
2016-09-27 22:21:29 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+using Matrix = std::vector <std::vector<T>>;
+auto i = 0;
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
